6aaa44e72fc8391b91e1a53ec8c0f71015dde6ca
Nabeel V <nabvali98@gmail.com>
2019-05-21 19:59:58 -0400

    Contact model is now a bare datamodel, updated the model usage in contact service

--- src/app/services/contact-storage.service.ts
+++ src/app/services/contact-storage.service.ts
@@ -1,12 +1,20 @@
-import { ContactModel } from '../shared/contact.model';
+import { ContactModel } from "../shared/contact.model";
 
 export class ContactStorageService {
-    contacts: ContactModel[] = [
-        new ContactModel('Test', 'Tester'),
-    ];
-    
-    addNewContact(newContact: ContactModel) {
-        this.contacts.push(newContact);
-        console.log(this.contacts);
+  contacts: ContactModel[] = [
+    {
+      firstName: "Test",
+      lastName: "Tester",
+      occupation: "Software Engineer",
+      phoneNumber: "616-442-3233",
+      emailAddress: "test@test.com",
+      streetAddress: "123 Awesome St"
+      // birthDate: "01/01/1998"
     }
+  ];
+
+  addNewContact(newContact: ContactModel) {
+    this.contacts.push(newContact);
+    console.log(this.contacts);
+  }
 }
--- src/app/shared/contact.model.ts
+++ src/app/shared/contact.model.ts
@@ -1,18 +1,25 @@
 export class ContactModel {
-    private firstName: string = "";
-    private lastName: string = "";
+  // private firstName: string = "";
+  // private lastName: string = "";
 
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  // constructor(firstName: string, lastName: string) {
+  //     this.firstName = firstName;
+  //     this.lastName = lastName;
+  // }
 
-    getFirstName() {
-        return this.firstName;
-    }
+  // getFirstName() {
+  //     return this.firstName;
+  // }
 
-    getLastName() {
-        return this.lastName;
-    }
+  // getLastName() {
+  //     return this.lastName;
+  // }
+
+  firstName: string;
+  lastName: string;
+  occupation: string;
+  phoneNumber: string;
+  emailAddress: string;
+  streetAddress: string;
+  // birthDate: string | Date;
 }
-
