1cfe5f967678525e82a423736f0e036733eb575b
Nabz786 <nabvali98@gmail.com>
2021-11-21 20:09:39 -0500

    configured environment configs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  contactsApiUrl: "https://localhost:44339/contacts",
+  authenticationApiUrl: "https://localhost:44339/userauthentication",
 };
 
 /*
