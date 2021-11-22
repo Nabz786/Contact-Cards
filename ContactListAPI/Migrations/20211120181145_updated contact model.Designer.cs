8daeb72504b9ef305d9ef24b0ba138dada9896e8
Nabz786 <nabvali98@gmail.com>
2021-11-21 20:05:15 -0500

    added migrations adding the remainder of the model properties for a contact

--- ContactListAPI/Migrations/20211120181145_updated contact model.Designer.cs
+++ ContactListAPI/Migrations/20211120181145_updated contact model.Designer.cs
@@ -4,14 +4,16 @@ using ContactListAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContactListAPI.Migrations
 {
     [DbContext(typeof(ContactsContext))]
-    partial class ContactsContextModelSnapshot : ModelSnapshot
+    [Migration("20211120181145_updated contact model")]
+    partial class updatedcontactmodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ContactListAPI/Migrations/20211120181145_updated contact model.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactListAPI.Migrations
+{
+    public partial class updatedcontactmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Occupation",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreetAddress",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Occupation",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "StreetAddress",
+                table: "Contacts");
+        }
+    }
+}
