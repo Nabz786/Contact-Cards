3b29f24132c55c0dfe9974fdc464cd80d316c52e
Nabz786 <nabvali98@gmail.com>
2021-11-21 20:08:31 -0500

    added logout button to header and removed router links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { AddContactComponent } from '../contact-list/add-contact/add-contact.component';
+import { LoginStatusSubjectService } from '../services/login-status.subject.service';
+import { UserSessionService } from '../services/usersession.service';
 
 @Component({
     selector: 'app-header',
@@ -9,12 +11,19 @@ import { AddContactComponent } from '../contact-list/add-contact/add-contact.com
 })
 export class HeaderComponent {
 
-    constructor(public dialog: MatDialog) { }
+    constructor(
+        public dialog: MatDialog,
+        private loginStatusSubjectService: LoginStatusSubjectService,
+        private userSessionService: UserSessionService) { }
 
-    openDialog(): void {
+    public openDialog(): void {
         this.dialog.open(AddContactComponent, { width: '500px', data: { isEdit: false } });
     }
 
+    public logout(): void {
+        this.loginStatusSubjectService.setLoginStatus(false);
+        this.userSessionService.logOut();
+    }
 }
 
 
