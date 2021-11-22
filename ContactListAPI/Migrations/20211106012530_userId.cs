97672d934a07d46b633c345ed90131f4dedac965
Nabz786 <nabvali98@gmail.com>
2021-11-21 20:04:53 -0500

    added migrations adding userid to contact model

---
+++ ContactListAPI/Migrations/20211106012530_userId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactListAPI.Migrations
+{
+    public partial class userId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Contacts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Contacts");
+        }
+    }
+}
