29414fa4e649ce45ce3f61b655796a1fd83cd648
Nabz786 <nabvali98@gmail.com>
2021-11-22 20:00:26 -0500

    removed contacts subject service init call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { ContactSubjectService } from './services/contact-subject.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'ContactList';
-  
-  constructor(private contactSubjectService: ContactSubjectService) { }
 
-  ngOnInit(): void {
-    this.contactSubjectService.getContacts();
-  }
+  constructor() { }
 }
