15d05d1537e72a025eeb787153f097ffa6f07f4e
Nabz786 <nabvali98@gmail.com>
2022-09-14 19:46:25 -0400

    Added button for user to delete their account

--- ContactListWebApp/src/app/header/header.component.ts
+++ ContactListWebApp/src/app/header/header.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material';
 import { AddContactComponent } from '../contact-list/add-contact/add-contact.component';
 import { LoginStatusSubjectService } from '../services/login-status.subject.service';
 import { UserSessionService } from '../services/usersession.service';
+import { UserDeleteAccountComponent } from '../user/delete-account/user-delete-account.component';
 
 @Component({
     selector: 'app-header',
@@ -23,6 +24,10 @@ export class HeaderComponent {
     public logout(): void {
         this.loginStatusSubjectService.setLoginStatus(false);
         this.userSessionService.logOut();
+    }
+
+    public deleteAccount(): void {
+        this.dialog.open(UserDeleteAccountComponent, { width: '500px'});
     }
 }
 
